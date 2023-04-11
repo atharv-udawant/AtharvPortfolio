6f8d3cca19ddffabdd671d3c9eeb528499f7e973
Anuj <anujbarave@gmail.com>
2023-04-12 00:03:08 +0530

    second commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer container-fluid">
+    <span>Developed By Atharv Udawant | &#169; 2023 All rights reserved.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterBlock",
+  mounted() {
+    AOS.init({
+      duration: 1200,
+      easing: "ease",
+      once: false,
+    });
+  },
+};
+</script>
+
+<style scoped>
+.footer {
+  text-align: center;
+  color: white;
+  padding: 20px;
+  background-color: #000018;
+}
+</style>
